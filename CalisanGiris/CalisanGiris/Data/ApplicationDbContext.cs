02c04cc616d41fa3dea8c8e0e9fd17abdb614937
Tahakalem <mahmuttahakalem@gmail.com>
2024-01-31 22:22:50 +0300

    Final ödevim
    Personel çalışma saatleri takip sistemi

---
+++ CalisanGiris/CalisanGiris/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalisanGiris.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
